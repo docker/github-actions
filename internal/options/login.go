cb5ad12e428aab60ceaf3068f2dcc83f4b92e3d3
Nick Adcock <nick.adcock@docker.com>
2020-03-04 09:21:36 +0000

    Error is username or password set but not both
    Signed-off-by: Nick Adcock <nick.adcock@docker.com>

--- internal/options/login.go
+++ internal/options/login.go
@@ -1,6 +1,10 @@
 package options
 
-import "github.com/caarlos0/env/v6"
+import (
+	"errors"
+
+	"github.com/caarlos0/env/v6"
+)
 
 // Login contains the parsed login action environment variables
 type Login struct {
@@ -8,9 +12,19 @@ type Login struct {
 	Password string `env:"INPUT_PASSWORD"`
 }
 
+var errLoginVarValidation = errors.New("both username and password must be set to login")
+
 // GetLoginOptions gets the login action environment variables
 func GetLoginOptions() (Login, error) {
 	var login Login
-	err := env.Parse(&login)
-	return login, err
+	if err := env.Parse(&login); err != nil {
+		return login, err
+	}
+
+	if login.Username != "" && login.Password == "" ||
+		login.Username == "" && login.Password != "" {
+		return login, errLoginVarValidation
+	}
+
+	return login, nil
 }
--- internal/options/login_test.go
+++ internal/options/login_test.go
@@ -16,3 +16,21 @@ func TestGetLoginOptions(t *testing.T) {
 	assert.Equal(t, "username", o.Username)
 	assert.Equal(t, "password", o.Password)
 }
+
+func TestLoginErrorNoPassword(t *testing.T) {
+	_ = os.Setenv("INPUT_USERNAME", "username")
+	_ = os.Unsetenv("INPUT_PASSWORD")
+
+	_, err := GetLoginOptions()
+
+	assert.Equal(t, err, errLoginVarValidation)
+}
+
+func TestLoginErrorNoUsername(t *testing.T) {
+	_ = os.Setenv("INPUT_PASSWORD", "password")
+	_ = os.Unsetenv("INPUT_USERNAME")
+
+	_, err := GetLoginOptions()
+
+	assert.Equal(t, err, errLoginVarValidation)
+}
