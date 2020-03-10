9553c8a804e074dbe5b9519ac89e5e3329a6d21c
Nick Adcock <nick.adcock@docker.com>
2020-03-10 12:22:27 +0000

    run buildx from binary
    Signed-off-by: Nick Adcock <nick.adcock@docker.com>

--- internal/command/runner.go
+++ internal/command/runner.go
@@ -57,13 +57,6 @@ func RunPush(cmd Runner, tags []string) error {
 
 // RunBuildX runs a docker build using buildx
 func RunBuildX(cmd Runner, opt options.Build, github options.GitHub, tags []string, push bool) error {
-	if err := cmd.Run("/docker-buildx"); err != nil {
-		return err
-	}
-	if err := cmd.Run("/docker-buildx", "create", "--help"); err != nil {
-		return err
-	}
-
 	fmt.Println("Creating buildx builder")
 	createArgs := BuildxCreateArgs()
 	if err := cmd.Run("/docker-buildx", createArgs...); err != nil {
